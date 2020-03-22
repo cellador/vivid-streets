78f16913763571a2425184787fe0030075a330e2
cellador <moritzkirschner95@gmail.com>
2020-03-22 18:47:26 +0100

    Successfully imported google maps API, speed up docker.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { CONFIG } from './config.js';
-// import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from 'google-map-react';
 
 export default function App() {
-  main();
   console.log("Test");
   return(null);
 }
