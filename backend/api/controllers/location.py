70a1e983168b1652e8de83fe51e67ed2725d689e
cellador <moritzkirschner95@gmail.com>
2020-04-07 22:09:24 +0200

    Started refactoring.

--- backend/api/controllers/location.py
+++ backend/api/controllers/location.py
@@ -4,97 +4,137 @@ from api import app, mongo
 import os
 import logger
 
-
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(__name__, filename=os.path.join(ROOT_PATH, 'output.log'))
 
 LOG.info("Loaded api.controllers.location.py")
 
 
-@app.route('/location_public', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-def location_public():
+@app.route('/location', methods=['GET', 'POST', 'DELETE', 'PATCH'])
+def location():
+    """Handle public and staged locations.
+
+    We rely on the 'collection' request arg to define which collection (public/staged) is
+    spoken to. We then pass the request to the proper collection.
+    """
+    # Enforce collection
+    requestArgs = request.args
+
+    try:
+        collectionArg = requestArgs.get('collection')
+
+        if collectionArg is not "public" and collectionArg is not "staged":
+            raise AttributeError()
+    except AttributeError as err:
+        return jsonify(
+            'Invalid URI parameter "collection". \
+             Values allowed: "public" and "staged"'), 400
+
+    if collectionArg is "public":
+        collection = mongo.db.loc_public
+    else:
+        collection = mongo.db.loc_staged
+
     if request.method == 'GET':
-        requestArgs = request.args
-
-        try:
-            if requestArgs.get('queryType') is None:
-                return jsonify('queryType not specified you motherfucker! Specify either queryType=loc or label to perform a query')
-        except AttributeError as err:
-            return jsonify('No arguments passed: Please pass queryType and latMin/Max, longMin/Max')
-
-        if requestArgs.get('queryType') == "loc":
-
-            # get locations from db
-            if requestArgs.get('latMax') is not None and \
-               requestArgs.get('latMin') is not None and \
-               requestArgs.get('longMax') is not None and \
-               requestArgs.get('longMin') is not None:
-                data = list(mongo.db.loc_public.find(
-                    {'latitude': {'$lte': float(requestArgs.get('latMax')),
-                                  '$gte': float(requestArgs.get('latMin'))},
-                     'longitude': {'$lte': float(requestArgs.get('longMax')),
-                                   '$gte': float(requestArgs.get('longMin'))}}))
-            elif requestArgs.get('latMax') is not None and \
-              requestArgs.get('latMin') is not None and \
-              requestArgs.get('longMax') is None and \
-              requestArgs.get('longMin') is None:
-                data = list(mongo.db.loc_public.find(
-                    {'latitude': {'$lte': float(requestArgs.get('latMax')),
-                                  '$gte': float(requestArgs.get('latMin'))}}))
-            elif requestArgs.get('latMax') is None and \
-              requestArgs.get('latMin') is None and \
-              requestArgs.get('longMax') is not None and \
-              requestArgs.get('longMin') is not None:
-                data = list(mongo.db.loc_public.find(
-                    {'longitude': {'$lte': float(requestArgs.get('longMax')),
-                                   '$gte': float(requestArgs.get('longMin'))}}))
-            return jsonify(data), 200
-
-        elif requestArgs.get('queryType') == "label":
-            label = requestArgs['label']
-            if label is not None:
-                data = list(mongo.db.loc_public.find({'label': label}))
-            return jsonify(data), 200
-
-        else:
-            return jsonify("Invalid queryType specified, asshole! Specify either 'loc' \
-                            or 'label' to perform a query"), 400
+        return _GET(requestArgs, collection)
 
     data = request.get_json()
     if request.method == 'POST':
-        if data.get('latitude', None) is not None and data.get('longitude', None) is not None:
-            mongo.db.loc_public.insert_one(data)
-            return jsonify({'ok': True, 'message': 'Location created successfully!'}), 200
-        else:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+        return _POST(data, collection)
 
     if request.method == 'DELETE':
-        if data.get('latitude', None) is not None:
-            db_response = mongo.db.loc_public.delete_one({'latitude': data['latitude']})
-            if db_response.deleted_count == 1:
-                response = {'ok': True, 'message': 'record deleted'}
-            else:
-                response = {'ok': True, 'message': 'no record found'}
-            return jsonify(response), 200
-        else:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
-        if data.get('longitude', None) is not None:
-            db_response = mongo.db.loc_public.delete_one({'longitude': data['longitude']})
-            if db_response.deleted_count == 1:
-                response = {'ok': True, 'message': 'record deleted'}
-            else:
-                response = {'ok': True, 'message': 'no record found'}
-            return jsonify(response), 200
-        else:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+        return _DELETE(data, collection)
 
     if request.method == 'PATCH':
-        if data.get('query', {}) != {}:
-            mongo.db.loc_public.update_one(
-                data['query'], {'$set': data.get('payload', {})})
-            return jsonify({'ok': True, 'message': 'record updated'}), 200
+        return _PATCH(data, collection)
+
+
+def _GET(requestArgs, collection):
+    # Enforce queryType
+    try:
+        queryType = requestArgs.get('queryType')
+
+        if queryType is not "loc" and queryType is not "label":
+            raise AttributeError()
+    except AttributeError as err:
+        return jsonify(
+            'Invalid URI parameter "queryType". \
+             Values allowed: "loc" and "label"'), 400
+
+    if queryType == "loc":
+        # get locations from db
+        if requestArgs.get('latMax') is not None and \
+           requestArgs.get('latMin') is not None and \
+           requestArgs.get('longMax') is not None and \
+           requestArgs.get('longMin') is not None:
+            data = list(collection.find(
+                {'latitude': {'$lte': float(requestArgs.get('latMax')),
+                              '$gte': float(requestArgs.get('latMin'))},
+                 'longitude': {'$lte': float(requestArgs.get('longMax')),
+                               '$gte': float(requestArgs.get('longMin'))}}))
+        elif requestArgs.get('latMax') is not None and \
+          requestArgs.get('latMin') is not None and \
+          requestArgs.get('longMax') is None and \
+          requestArgs.get('longMin') is None:
+            data = list(collection.find(
+                {'latitude': {'$lte': float(requestArgs.get('latMax')),
+                              '$gte': float(requestArgs.get('latMin'))}}))
+        elif requestArgs.get('latMax') is None and \
+          requestArgs.get('latMin') is None and \
+          requestArgs.get('longMax') is not None and \
+          requestArgs.get('longMin') is not None:
+            data = list(collection.find(
+                {'longitude': {'$lte': float(requestArgs.get('longMax')),
+                               '$gte': float(requestArgs.get('longMin'))}}))
         else:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+            data = list(collection.find())
+        return jsonify(data), 200
+
+    elif queryType == "label":
+        label = requestArgs['label']
+        if label is not None:
+            data = list(collection.find({'label': label}))
+        else:
+            data = list(collection.find())
+        return jsonify(data), 200
+
+
+def _POST(data, collection):
+    if data.get('latitude', None) is not None and data.get('longitude', None) is not None:
+        collection.insert_one(data)
+        return jsonify({'ok': True, 'message': 'Location created successfully!'}), 200
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+
+
+def _DELETE(data, collection):
+    if data.get('latitude', None) is not None:
+        db_response = collection.delete_one({'latitude': data['latitude']})
+        if db_response.deleted_count == 1:
+            response = {'ok': True, 'message': 'record deleted'}
+        else:
+            response = {'ok': True, 'message': 'no record found'}
+        return jsonify(response), 200
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+    if data.get('longitude', None) is not None:
+        db_response = collection.delete_one({'longitude': data['longitude']})
+        if db_response.deleted_count == 1:
+            response = {'ok': True, 'message': 'record deleted'}
+        else:
+            response = {'ok': True, 'message': 'no record found'}
+        return jsonify(response), 200
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+
+
+def _PATCH(data, collection):
+    if data.get('query', {}) != {}:
+        mongo.db.loc_public.update_one(
+            data['query'], {'$set': data.get('payload', {})})
+        return jsonify({'ok': True, 'message': 'record updated'}), 200
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
 #staged location api endpoints
 @app.route('/location_staged', methods=['GET', 'POST', 'DELETE', 'PATCH'])
@@ -102,7 +142,7 @@ def location_staged():
     if request.method == 'GET':
         requestArgs = request.args
         # get staged locations from db
-        data = list(mongo.db.loc_staged.find())      
+        data = list(mongo.db.loc_staged.find())
         return jsonify(data), 200
 
 
@@ -123,7 +163,7 @@ def location_staged():
                 mongo.db.loc_staged.insert_one({'label': data['label'],
                                      'latitude': data['latitude'],
                                      'longitude': data['longitude'],
-                                     'user_id': data['user_id']}) 
+                                     'user_id': data['user_id']})
             return jsonify({'ok': True, 'message': 'Location created successfully!'}), 200
         else:
             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
